ff08b7c7e0b17e3b114819eaba8fb54571cd3a78
JohnDoe2525 <naotakenoguchi@gmail.com>
2023-11-27 20:39:01 +0900

    create

---
+++ src/test/java/example/noguchi/portfolio/OriginalApplicationTests.java
@@ -0,0 +1,13 @@
+package example.noguchi.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
