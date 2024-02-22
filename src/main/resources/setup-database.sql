ecdd8dc24c80c1cbb9e10c574f00e23c793821f7
JohnDoe2525 <naotakenoguchi@gmail.com>
2024-02-22 22:25:24 +0900

    VPSでの動作確認完了

--- src/main/resources/setup-database.sql
+++ src/main/resources/setup-database.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE gaman_banking CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER 'bankuser'@'localhost' IDENTIFIED BY 'bankpass';
-GRANT ALL PRIVILEGES ON gaman_banking.* to 'bankuser'@'localhost';
+CREATE USER 'bankuser'@'%' IDENTIFIED BY 'bankpass';
+GRANT ALL PRIVILEGES ON gaman_banking.* to 'bankuser'@'%';
