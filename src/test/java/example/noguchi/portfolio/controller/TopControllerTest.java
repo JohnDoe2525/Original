a180c78c2af167f2a22735c2b662b0e16e6887b1
naotake_vs <naotakenoguchi@gmail.com>
2024-04-07 23:27:53 +0900

    未登録ユーザーの認可のテストコードを修正

--- src/test/java/example/noguchi/portfolio/controller/TopControllerTest.java
+++ src/test/java/example/noguchi/portfolio/controller/TopControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -53,6 +54,7 @@ class TopControllerTest {
     }
 
     @Test
+    @WithAnonymousUser
     @DisplayName("未登録のユーザーがログインしようとした場合(認可)")
     public void testTop02() throws Exception {
         mockMvc.perform(get("/"))
