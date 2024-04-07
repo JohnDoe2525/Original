e7ded60256b1def0743e2eb837aac08463f29358
naotake_vs <naotakenoguchi@gmail.com>
2024-04-07 19:04:49 +0900

    ログイン後の画面遷移のテスト実装

--- src/test/java/example/noguchi/portfolio/controller/TopControllerTest.java
+++ src/test/java/example/noguchi/portfolio/controller/TopControllerTest.java
@@ -1,11 +1,11 @@
 package example.noguchi.portfolio.controller;
 
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,27 +14,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 class TopControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @DisplayName("ログイン画面へのアクセス結果")
     public void testLogin() throws Exception {
         mockMvc.perform(get("/login"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"));
     }
 
-//    @Test
-//    public void testTop() throws Exception {
-//        fail("まだ実装されていません");
-//    }
+    @Test
+    @WithMockUser
+    @DisplayName("登録済みのユーザーがログインしようとした場合")
+    public void testTop01() throws Exception {
+        mockMvc.perform(get("/"))
+        .andExpect(redirectedUrl("/gamanbanking/home"));
+    }
+
+    @Test
+    @DisplayName("未登録のユーザーがログインしようとした場合")
+    public void testTop02() throws Exception {
+        mockMvc.perform(get("/"))
+        .andExpect(redirectedUrl("http://localhost/login"));
+    }
 
 }
