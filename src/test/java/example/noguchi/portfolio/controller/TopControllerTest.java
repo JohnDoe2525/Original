6f57394e8d10afc1632a26bcdb0b0392eabfb148
naotake_vs <naotakenoguchi@gmail.com>
2024-04-07 19:36:00 +0900

    TopControllerクラスのテストクラスの不要なアノテーションを削除

--- src/test/java/example/noguchi/portfolio/controller/TopControllerTest.java
+++ src/test/java/example/noguchi/portfolio/controller/TopControllerTest.java
@@ -7,17 +7,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
 class TopControllerTest {
     @Autowired
     private MockMvc mockMvc;
