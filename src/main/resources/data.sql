42dd4dfb9d74bbdf51bd377b5893da83762b33bd
naotake_vs <naotakenoguchi@gmail.com>
2024-04-15 08:51:06 +0900

    README作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO gaman_banking.users(id,name,role,password,delete_flg,created_at,updated_at)
-     VALUES (null,"admin","ADMIN","$2a$10$vY93/U2cXCfEMBESYnDJUevcjJ208sXav23S.K8elE/J6Sxr4w5jO",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES (null,"admin","ADMIN","$2a$10$dKQbMmg495yGK7mE33Vlg.K/rdlQLeEHvHRD5NDpviw9dFaBmfgEO",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO gaman_banking.users(id,name,role,password,delete_flg,created_at,updated_at)
      VALUES (null,"taro","GENERAL","$2a$10$HPIjRCymeRZKEIq.71TDduiEotOlb8Ai6KQUHCs4lGNYlLhcKv4Wi",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO gaman_banking.users(id,name,role,password,delete_flg,created_at,updated_at)
@@ -7,11 +7,13 @@ INSERT INTO gaman_banking.users(id,name,role,password,delete_flg,created_at,upda
 INSERT INTO gaman_banking.categories(id,name,delete_flg,created_at,updated_at)
      VALUES (null,"お菓子",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO gaman_banking.categories(id,name,delete_flg,created_at,updated_at)
-     VALUES (null,"ジュース",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES (null,"飲み物",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO gaman_banking.categories(id,name,delete_flg,created_at,updated_at)
-     VALUES (null,"ラーメン",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES (null,"外食",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO gaman_banking.categories(id,name,delete_flg,created_at,updated_at)
-     VALUES (null,"タバコ",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES (null,"飲み会",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO gaman_banking.categories(id,name,delete_flg,created_at,updated_at)
+     VALUES (null,"電車",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO gaman_banking.transactions(transaction_id,transaction_date,price,category_id,memo,user_id,delete_flg,created_at,updated_at)
      VALUES (null,CURRENT_TIMESTAMP,300,1,"じゃがりこ",1,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO gaman_banking.transactions(transaction_id,transaction_date,price,category_id,memo,user_id,delete_flg,created_at,updated_at)
